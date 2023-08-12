752ae6ad3e723a2dbb0de5778f8c8026fac2a5c2
Rajkumar-DataEngineer <raj_kumar15@outlook.com>
2023-08-12 11:23:16 +0530

    my solution

---
+++ Zen_id_Raj_solution/solution.py
@@ -0,0 +1,35 @@
+
+print("python basic data types")
+a = 5
+print("Type of a: ", type(a))
+
+b = 5.0
+print("Type of b: \n", type(b))
+
+d = "hello"
+print("\nTypde od d:", type(d))
+
+c = 2 + 4j 
+print("\nType of c: ", type(c))
+
+
+#boolean -> TRUE or FALSE
+str1='hi'
+print('i' in str1) 
+
+print("data type conversion")
+#int -> string 
+
+x=str(a)
+print(x)
+
+#int -> float 
+y=float(a)
+print(x)
+
+#int -> array 
+
+#string -> list 
+
+
+
