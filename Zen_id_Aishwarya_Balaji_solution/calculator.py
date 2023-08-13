67ba3be9c89d16a556572a090121a0361d70772e
Aishwarya0206 <aishustarz02@gmail.com>
2023-08-13 17:19:38 +0530

    Calculator

--- Zen_id_Aishwarya_Balaji_solution/calculator.py
+++ Zen_id_Aishwarya_Balaji_solution/calculator.py
@@ -1,32 +1,3 @@
-'''Q1)
-
-1. accept a string from user.
-2. check if the list is palindrome or not.
-3. create a class and object 
-    3.a) create a constructor. 
-4. access the logic of palindrome using object.method() '''
-
-#Class
-class my_class:
-  #Constructor
-  def __init__(self, my_str):
-    self.my_str = my_str
-
-  #Palindrome method
-  def check_palindrome(self):
-    return(f"{self.my_str} is palindrome" if(self.my_str == self.my_str[::-1]) else f"{self.my_str} is not a palindrome")
-
-#Main
-if __name__ == '__main__':
-  #Get the string
-  get_str = input("Enter a string: ")
-
-  #Create an object for the class
-  obj = my_class(get_str)
-  #Call the methods
-  print(obj.check_palindrome())
-
-
 '''Q2) 
 
 1. create a class called calculator.
