84602b5eb446a43b3e6be3df2a81e94051c02c5b
Kannan Kandasamy <kannanvijay@hotmail.com>
2023-08-17 10:54:52 -0400

    Changed logic to index approach to one liner

--- Zen_id_kannan_solution/week3_q2_index.py
+++ Zen_id_kannan_solution/week3_q2_index.py
@@ -6,17 +6,14 @@
   Details :   Getting an index position of a number using looping
 """
 class Numbers:
-  def __init__(self, n):
+  def __init__(self, n=0):
     self.num = n
 
   def get_index(self, lis, n):
-    for i in enumerate(lis):
-      if i[1]==n:
-        return i[0]
-        break
-    return -1
+    op = next((i for i,val in enumerate(lis) if val==n),-1)
+    return op
 
-  def get_index(self, lis):
+  def get_index_manual(self, lis):
     for i in enumerate(lis):
       if i[1]==self.num:
         return i[0]
@@ -28,8 +25,8 @@ if __name__=="__main__":
   try:
     ip = list(map(int,input("Enter list of numbers in space separated : ").split()))
     n = int(input("Enter number to find : "))
-    num_obj = Numbers(n)
-    op = num_obj.get_index(ip)
+    num_obj = Numbers()
+    op = num_obj.get_index(ip,n)
     print("Not found" if op==-1 else "Found at "+str(op))    
   except:
     print("Enter valid numbers")
