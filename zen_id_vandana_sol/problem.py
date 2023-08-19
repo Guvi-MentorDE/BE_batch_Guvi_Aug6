a8e9e0e675a7eabf1f77f2e8efd604bad909ba05
vandana5singh <vandanas2685@gmail.com>
2023-08-19 10:22:35 +0530

    Assign_sol

---
+++ zen_id_vandana_sol/problem.py
@@ -0,0 +1,21 @@
+"""its working in collab ---------------------------Querry
+"""
+class NUM:
+    x=[int(i) for i in input("enter string of numbers").split(",")]
+    def __init__(self):
+        self.even_num=[]
+        self.odd_num=[]
+    def even(self):
+        for i in range(len(x)):
+            if x[i]%2==0:
+                self.even_num.append(x[i])
+        return self.even_num
+    def odd(self):
+        for i in range(len(x)):
+            if x[i]%2!=0:
+                self.odd_num.append(x[i])
+        return self.odd_num
+if __name__=="__main__":
+    object1=NUM()
+    print(object1.even())
+    print(object1.odd())
