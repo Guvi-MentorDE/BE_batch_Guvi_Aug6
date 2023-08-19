e6cdb4efa941a46343ef9e86facaed596cd51c2b
ramshankarbalakrishnan <ramshankarcse@gmail.com>
2023-08-19 11:00:24 +0530

    Assignment_aug13_upd

--- Zen_id_Ramshankar/Assignment_aug13_array_search.py
+++ Zen_id_Ramshankar/Assignment_aug13_array_search.py
@@ -1,16 +1,26 @@
 import math
 
+class Array_search:
+    def  __init__(self,arr):
+        self.arr=arr
+    
+    def search(self):
+        count=1
+        flag=0
+        for i in self.arr:
+            if(i==5):
+                print("We found the number 5 in position",count,"of the input")
+                flag=1
+            count=count+1
+        if(flag==0):
+            print("There are no occurrences of number 5 in the input")
+
+
+
 if __name__=="__main__":
     print("Enter input of numbers seperated by comma:")
     input_elements=list(map(int,input().split(',')))
-    print(input_elements)
-    count=1
-    flag=0
-    for i in input_elements:
-        if(i==5):
-            print("We found the number 5 in position",count,"of the input")
-            flag=1
-        count=count+1
-    if(flag==0):
-        print("There are no occurrences of number 5 in the input")
+    array_obj=Array_search(input_elements)
+    array_obj.search()
+    
     
--- Zen_id_Ramshankar/Assignment_aug13_dictionary.py
+++ Zen_id_Ramshankar/Assignment_aug13_dictionary.py
@@ -1,11 +1,20 @@
 import math
 
+class Dict:
+    def __init__(self,dict_inp1,dict_inp2):
+        self.dict_inp1=dict_inp1
+        self.dict_inp2=dict_inp2
+
+    def intersect(self):
+        dict_common={}
+        for key1,val1 in dict1.items():
+            for key2,val2 in dict2.items():
+                if(key1==key2):
+                    dict_common[key1]=val1
+        return dict_common
+
 if __name__=="__main__":
     dict1 ={"INDIA":"INR","US":"USD"}
     dict2 ={"INDIA":"INR","UK":"GBP","JAPAN":"YEN"}
-    dict_common={}
-    for key1,val1 in dict1.items():
-        for key2,val2 in dict2.items():
-            if(key1==key2):
-                dict_common[key1]=val1
-    print("The common keys between both the dictionary values",dict_common)
+    dict_obj=Dict(dict1,dict2)
+    print("The common keys between both the dictionary values",dict_obj.intersect())
