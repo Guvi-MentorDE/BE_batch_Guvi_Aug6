059239649615a05161a560fd624b78f88bb61921
vandana5singh <vandanas2685@gmail.com>
2023-08-21 14:40:52 +0530

    Assign sol

--- zen_id_vandana_sol/odd_even_using_global.py
+++ zen_id_vandana_sol/odd_even_using_global.py
@@ -2,18 +2,19 @@
 """
 class NUM:
     x=[int(i) for i in input("enter string of numbers").split(",")]
+    
     def __init__(self):
         self.even_num=[]
         self.odd_num=[]
     def even(self):
-        for i in range(len(x)):
-            if x[i]%2==0:
-                self.even_num.append(x[i])
+        for i in range(len(self.x)):
+            if self.x[i]%2==0:
+                self.even_num.append(self.x[i])
         return self.even_num
     def odd(self):
-        for i in range(len(x)):
-            if x[i]%2!=0:
-                self.odd_num.append(x[i])
+        for i in range(len(self.x)):
+            if self.x[i]%2!=0:
+                self.odd_num.append(self.x[i])
         return self.odd_num
 if __name__=="__main__":
     object1=NUM()
