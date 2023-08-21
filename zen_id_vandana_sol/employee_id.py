5f5575502468c3b1f7a04295c3dff74eda72557c
vandana5singh <vandanas2685@gmail.com>
2023-08-21 15:38:58 +0530

    assign sol

--- zen_id_vandana_sol/employee_id.py
+++ zen_id_vandana_sol/employee_id.py
@@ -9,10 +9,10 @@ class Employee:
       self.name=Name
     
 
-
-Employee_1=Employee(20,"vipin")
-Employee_2=Employee(23,"kajal")
-Employee_1.id="25"
-Employee_2.id="100"
-print("ist employee","id:",Employee_1.id,"Age:",Employee_1.age, "Name:",Employee_1.name)
-print("2nd employee","id:",Employee_2.id,"Age:",Employee_2.age, "Name:",Employee_2.name)
+if __name__=="__main__":
+  Employee_1=Employee(20,"vipin")
+  Employee_2=Employee(23,"kajal")
+  Employee_1.id="25"
+  Employee_2.id="100"
+  print("ist employee","id:",Employee_1.id,"Age:",Employee_1.age, "Name:",Employee_1.name)
+  print("2nd employee","id:",Employee_2.id,"Age:",Employee_2.age, "Name:",Employee_2.name)
