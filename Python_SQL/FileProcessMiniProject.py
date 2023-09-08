68017c245f18fe8a097d530eac2a20b48dcd2009
Rajkumar-DataEngineer <raj_kumar15@outlook.com>
2023-09-09 00:05:06 +0530

    class notes for sep9

--- Python_SQL/FileProcessMiniProject.py
+++ Python_SQL/FileProcessMiniProject.py
@@ -9,10 +9,18 @@ def connect_db(db):
 def execute_ddl(cursor_db):
     print(execute_ddl)
     #cursor_db.execute("CREATE TABLE IF NOT EXISTS customers (cust_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
-    sql ='''CREATE TABLE IF NOT EXISTS orders (order_uniq_id INT AUTO_INCREMENT PRIMARY KEY, order_id varchar(50), cust_id varchar(50), product varchar(255), country varchar(255) , order_status varchar(50), order_amount varchar(50))'''
-    sql1 ='''CREATE TABLE IF NOT EXISTS orders_v2 (order_uniq_id INT , order_id INT, cust_id INT, product INT, country varchar(50) , order_status varchar(50), order_amount double)'''
+    drop_orders = '''drop table if EXISTS orders;'''
+    drop_orders_v2 = '''drop table if EXISTS orders_v2;'''
+    drop_orders_sales = '''drop table if EXISTS orders_sales;'''
+    sql =''' CREATE TABLE IF NOT EXISTS orders (order_uniq_id INT AUTO_INCREMENT PRIMARY KEY, order_id varchar(50), cust_id varchar(50), product varchar(255), country varchar(255) , order_status varchar(50), order_amount varchar(50));'''
+    sql1 =''' CREATE TABLE IF NOT EXISTS orders_v2 (order_uniq_id INT , order_id INT, cust_id INT, product INT, country varchar(50) , order_status varchar(50), order_amount double);'''
+    sql2 =''' CREATE TABLE IF NOT EXISTS orders_sales (order_id INT, total_sales double);'''
+    cursor_db.execute(drop_orders)
+    cursor_db.execute(drop_orders_v2)
+    cursor_db.execute(drop_orders_sales)
     cursor_db.execute(sql)
     cursor_db.execute(sql1)
+    cursor_db.execute(sql2)
     db_connection.commit() #save 
     #db_connection.rollback() #undo
     cursor_db.close()
@@ -48,8 +56,14 @@ def select_result(cursor_db,db_connection):
     for x in myresult:
         print(x)
     close_connection(cursor_db,db_connection)
+    
+def find_sales(cursor_db,db_connection):
+    query = '''insert into orders_sales (order_id,total_sales) select order_id, total_sales from(select *, sum(order_amount) over(partition by order_id order by cust_id) as total_sales  from orders_v2)tmp where tmp.order_status="completed"; '''
+    cursor_db.execute(query)
+    db_connection.commit()
+    close_connection(cursor_db,db_connection)
+    
         
-
 def close_connection(cursor_db,db_connection):
     db_connection.commit()
     cursor_db.close()
@@ -66,3 +80,6 @@ if __name__ == "__main__":
     #select_result(cursor_db,db_connection)
     cursor_db,db_connection=connect_db(db)
     tansform_result(cursor_db,db_connection)
+    cursor_db,db_connection=connect_db(db)
+    find_sales(cursor_db,db_connection)
+    
