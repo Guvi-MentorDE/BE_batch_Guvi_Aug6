1a37e47e20484d126372c8373859a67a792c4d82
Aishwarya0206 <aishustarz02@gmail.com>
2023-09-12 01:55:45 +0530

    Completed python + sql assignment

--- Zen_id_Aishwarya_Balaji_solution/Python + SQL Assignments/data_handler_sql.py
+++ Zen_id_Aishwarya_Balaji_solution/Python + SQL Assignments/data_handler_sql.py
@@ -26,12 +26,15 @@ def execute_ddl(cursor_db):
     #cursor_db.execute("CREATE TABLE IF NOT EXISTS customers (cust_id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
     sql ='''CREATE TABLE IF NOT EXISTS orders (order_uniq_id INT AUTO_INCREMENT PRIMARY KEY, order_id varchar(50), cust_id varchar(50), product varchar(255), country varchar(255) , order_status varchar(50), order_amount varchar(50))'''
     sql1 ='''CREATE TABLE IF NOT EXISTS orders_v2 (order_uniq_id INT , order_id INT, cust_id INT, product INT, country varchar(50) , order_status varchar(50), order_amount double)'''
+    sql2 ='''CREATE TABLE IF NOT EXISTS orders_v3 (order_uniq_id INT , order_id INT, cust_id INT, country varchar(50) , order_status varchar(50), total_value_of_orders double)'''
+    #view_sql ='''CREATE TABLE IF NOT EXISTS orders_v4 (order_uniq_id INT , order_id INT, cust_id INT, country varchar(50) , order_status varchar(50), total_value_of_orders double)'''
+    cursor_db.execute(sql2)
     cursor_db.execute(sql)
     cursor_db.execute(sql1)
+    #cursor_db.execute(view_sql)
     db_connection.commit() #save 
     #db_connection.rollback() #undo
     cursor_db.close()
-
 
 def csv_read(cursor_db,db_connection):
     print(csv_read)
@@ -54,11 +57,22 @@ def tansform_result(cursor_db,db_connection):
     query = "insert into orders_v2 (order_uniq_id,order_id,cust_id,product,country,order_status,order_amount) SELECT order_uniq_id, cast(order_id as DECIMAL) as order_id , cast(cust_id as DECIMAL) as cust_id, cast(product as DECIMAL) as product, country, order_status, cast(order_amount as DECIMAL) as order_amount FROM orders"
     cursor_db.execute(query)
     db_connection.commit()
-    close_connection(cursor_db,db_connection)
+    #close_connection(cursor_db,db_connection)
+
+def tansform_result_from_orders_v2(cursor_db,db_connection):
+    query = "insert into orders_v3 (order_uniq_id,order_id,cust_id,country,order_status,total_value_of_orders) SELECT * FROM (SELECT order_uniq_id, order_id, cust_id, country, order_status, SUM(order_amount) OVER(PARTITION BY cust_id ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS total_value_of_orders FROM orders_v2) tmp WHERE order_status = 'completed';"
+    cursor_db.execute(query)
+    db_connection.commit()
+    
+def view_from_orders_v2(cursor_db,db_connection):
+    query = "CREATE VIEW orders_v4 AS SELECT * FROM (SELECT order_uniq_id, order_id, cust_id, country, order_status, SUM(order_amount) OVER(PARTITION BY cust_id ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS total_value_of_orders FROM orders_v2) tmp WHERE order_status = 'completed'"
+    cursor_db.execute(query)
+    db_connection.commit()
+    #close_connection(cursor_db,db_connection)
 
     
 def select_result(cursor_db,db_connection):
-    query = "SELECT * FROM orders"
+    query = "SELECT * FROM orders_v4"
     cursor_db.execute(query)
     myresult = cursor_db.fetchall()
     for x in myresult:
@@ -79,6 +93,8 @@ if __name__ == "__main__":
     close_connection(cursor_db,db_connection)
     cursor_db,db_connection=connect_db(db)
     csv_read(cursor_db,db_connection)
-    #select_result(cursor_db,db_connection)
     cursor_db,db_connection=connect_db(db)
     tansform_result(cursor_db,db_connection)
+    tansform_result_from_orders_v2(cursor_db, db_connection)
+    view_from_orders_v2(cursor_db, db_connection)
+    select_result(cursor_db,db_connection)
