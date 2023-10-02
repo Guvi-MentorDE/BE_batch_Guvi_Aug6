a36ff75663020d8c8a830385b8ddcb1272e4bff9
Abhinav Banerjee <banerjeeabhinav1998@gmail.com>
2023-10-02 22:12:14 +0530

    shell script updated

--- hadoop/hive/mini_project_hive_incr/daily_script.sh
+++ hadoop/hive/mini_project_hive_incr/daily_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 #exec_date=20230902
 exec_date=$1
-
 file_path=/home/prera/data/emp_data_${exec_date}.txt #make changes to this path according to your cluster folder structure
-
 hive --hiveconf file_path="${file_path}" -f /home/prera/bin/scd.hql  #make changes to this path according to your cluster folder structure
