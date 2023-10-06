73e8323741d2e38df51ce207a446785086da729d
Senthilkumar <46195896+vissenthil@users.noreply.github.com>
2023-10-06 08:38:27 +0800

    committed

--- Assignements/Zen_id_Vissenthilkumar/Python/Palindrome.py
+++ Assignements/Zen_id_Vissenthilkumar/Python/Palindrome.py
@@ -1,20 +1,25 @@
-def Find_Palindrome(Str):
-    ''' This function will check the given string
-        are palindrome or not '''
-    ln = len(Str) - 1
+class palindrome_check:
+    def __init__(self,checkword):
+        self.checkword = checkword
 
-    reversed_str = ''
+    def Find_Palindrome(self):
+        ''' This function will check the given string
+            are palindrome or not '''
+        ln = len(self.checkword) - 1
 
-    while ln >= 0:
-        reversed_str  = reversed_str + str[ln]
-        ln = ln - 1
+        reversed_str = ''
 
-    if  Str ==  reversed_str :
-        print('Input string is palidrome')
-    else:
-        print('Input string is not palidrome')
+        while ln >= 0:
+            reversed_str  = reversed_str + self.checkword[ln]
+            ln = ln - 1
+
+        if  self.checkword ==  reversed_str :
+            print('Input string is palidrome')
+        else:
+            print('Input string is not palidrome')
 
 
 if __name__ == '__main__':
-    str = input('Enter a string to check palindrome :')    
-    Find_Palindrome(str)
+    str = input('Enter a string to check palindrome :')   
+    check = palindrome_check(str) 
+    check.Find_Palindrome()
